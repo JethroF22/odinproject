4294230787bcf09b5d290acb205cf7fd0923efb4
Bruno Parga <brunoparga@gmail.com>
2017-07-01 19:47:03 -0400

    Establish the basic page outline

--- js/application.js
+++ js/application.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
+var order = 16;
 
+$(document).ready(function() {
+  $('button').click(function() {
+    order = $('input').val();
+  });
 });
+
+function drawCanvas(order) {
+  for (var i = 0; i < order; i++) {
+    drawLine(order);
+  }
+}
+
+function drawLine(order) {
+  for (var i = 0; i < order; i++) {
+    
+  }
+}
