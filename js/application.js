814c76232aa4e1936da7b2e5bcfe8762a09443b0
Bruno Parga <brunoparga@gmail.com>
2017-07-02 08:34:42 -0400

    Make the page work as requested

--- js/application.js
+++ js/application.js
@@ -1,19 +1,27 @@
-var order = 16;
-
 $(document).ready(function() {
+  var size = setSize();
+  drawGrid(size);
   $('button').click(function() {
-    order = $('input').val();
-  });
+    size = setSize();
+    drawGrid(size);
+  })
 });
 
-function drawCanvas(order) {
-  for (var i = 0; i < order; i++) {
-    drawLine(order);
+function drawGrid(size=order) {
+  $('#canvas').empty();
+  var $grid = $('<div></div>')
+  for (var i = 0; i < order*order; i++) {
+    $grid.append(cell.clone())
   }
+  $('#canvas').append($grid);
+  $('.cell').on('mouseenter', function() {
+    $(this).addClass('painted');
+  })
 }
 
-function drawLine(order) {
-  for (var i = 0; i < order; i++) {
-    
-  }
+function setSize () {
+  order = $('input').val();
+  each = (960 - 4 * order)/ order;
+  cell = $('<div class="cell" style="height: ' + each + 'px; width: ' + each + 'px"></div>')
+  return cell;
 }
