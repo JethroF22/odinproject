d4010f787cf37f41f370298ffe8b494e05744ef8
Bruno Parga <brunoparga@gmail.com>
2017-07-09 11:39:23 -0400

    Restore aility to create articles

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ApplicationRecord
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "Should match confirmation", if: :password
-  has_many :articles
 end
