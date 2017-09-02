411b017e00457094425f316df345522af7aa6c54
Bruno Parga <brunoparga@gmail.com>
2017-09-02 13:28:57 -0300

    Solve binary search tree problem

--- bst.rb
+++ bst.rb
@@ -3,9 +3,23 @@ class Node
 
   def initialize(value, left = nil, right = nil)
     @value = value
-    @parent
     @left = left
     @right = right
+  end
+
+  def print
+    if @parent.nil?
+      print "ROOT"
+    else
+      print "Parent: #{@parent.value}"
+    end
+    puts "\nValue: #{@value}"
+    puts "Left child: #{@left.value}" unless @left.nil?
+    puts "Right child: #{@right.value}" unless @right.nil?
+  end
+
+  def to_s
+    @value
   end
 
 end
@@ -16,6 +30,7 @@ def build_tree(array)
     node = Node.new(element)
     insert(node, root)
   end
+  root
 end
 
 def insert(node, root)
@@ -27,10 +42,14 @@ def insert(node, root)
       root.left.parent = node
       node.left = root.left
       node.right = root
-      root.parent.right = node if node.value > root.parent.value
-      root.parent.left = node if node.value < root.parent.value
       node.parent = root.parent
+      if (not root.parent.nil?) and node.value > root.parent.value
+        root.parent.right = node
+      elsif (not root.parent.nil?) and node.value < root.parent.value
+        root.parent.left = node
+      end
       root.parent = node
+      root.left = nil
     else
       insert(node, root.left)
     end
@@ -42,14 +61,49 @@ def insert(node, root)
       root.right.parent = node
       node.right = root.right
       node.left = root
-      root.parent.right = node if node.value > root.parent.value
-      root.parent.left = node if node.value < root.parent.value
       node.parent = root.parent
+      if (not root.parent.nil?) and node.value > root.parent.value
+        root.parent.right = node
+      elsif (not root.parent.nil?) and node.value < root.parent.value
+        root.parent.left = node
+      end
       root.parent = node
+      root.right = nil
     else
       insert(node, root.right)
     end
   end
 end
 
-build_tree([3, 7, 8, 1, 14, 10, 2, 6, 11, 12, 5, 9, 15, 4, 13])
+def breadth_first_search(node, query)
+  queue = [node]
+  while queue.length > 0
+    return queue[0] if queue[0].value == query
+    queue << queue[0].left unless queue[0].left.nil?
+    queue << queue[0].right unless queue[0].right.nil?
+    queue.shift
+  end
+  nil
+end
+
+def depth_first_search(node, query)
+  stack = [node]
+  while stack.length > 0
+    last = stack.pop
+    return last if last.value == query
+    stack << last.right unless last.right.nil?
+    stack << last.left unless last.left.nil?
+  end
+  nil
+end
+
+def dfs_rec(node, query, found=nil)
+  if node.value == query
+    found = node
+  end
+  left = dfs_rec(node.left, query, found) unless node.left.nil? or found
+  found = left if not left.nil?
+  right = dfs_rec(node.right, query, found) unless node.right.nil? or found
+  found = right if not right.nil?
+  found
+end
