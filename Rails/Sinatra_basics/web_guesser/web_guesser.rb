ece2b42191bc0d07bb2a57ad451660e7a2d0fd02
Bruno Parga <brunoparga@gmail.com>
2017-10-11 21:43:36 -0300

    Rename the Ruby_on_Rails directory

--- Rails/Sinatra_basics/web_guesser/web_guesser.rb
+++ Rails/Sinatra_basics/web_guesser/web_guesser.rb
@@ -1,32 +1,49 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number, rand(100)
-
 get '/' do
   guess = params['guess'].to_i
-  message, bgcolor = check_guess(guess)
-  erb :index, locals: {number: settings.number, message: message, bgcolor: bgcolor}
+  check_guess(guess)
+  erb :index, locals: {number: settings.number, message: message, bgcolor: bgcolor, guesses: Guesser.guesses_remaining}
 end
 
-def check_guess(guess)
-  if settings.number == guess
-    bgcolor = "#33CC00"
-  elsif (settings.number - guess).abs < 5
-    bgcolor = "#FF3333"
-    way = 'Too'
-  else
-    bgcolor = "#FF0000"
-    way = 'Way too'
+class Guesser
+
+  def new_game
+    set :number, rand(100)
+    @@guesses_remaining = 5
   end
 
-  if guess > settings.number
-    message = "#{way} high!"
-  elsif guess < settings.number
-    message = "#{way} low!"
-  else
-    message = "You got it right!"
+  def check_guess(guess)
+    if settings.number == guess
+      bgcolor = "#33CC00"
+    elsif (settings.number - guess).abs < 5
+      bgcolor = "#FF3333"
+      way = 'Too'
+    else
+      bgcolor = "#FF0000"
+      way = 'Way too'
+    end
+
+    if guess > settings.number
+      message = "#{way} high!"
+    elsif guess < settings.number
+      message = "#{way} low!"
+    else
+      new_game
+      message = "You got it right! Try guessing a new number."
+    end
+    @@guesses_remaining -= 1
+    if @@guesses_remaining <= 0
+      lose
+    else
+      message += "\nYou have #{guesses_remaining} guesses left."
+    end
   end
 
-  return message, bgcolor
+  def lose
+    message += "\nYou lose! Try guessing a new number."
+    new_game
+  end
+
 end
